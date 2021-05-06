7ca9446295699533d8c27b378703fd45abbd5b86
Yunus Emre Halis <yemrehalis@gmail.com>
2021-05-06 20:22:06 +0300

    overview h4 and css added

--- app.js
+++ app.js
@@ -54,6 +54,8 @@ function showMovies(movies) {
                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>   
             <div class="overview">
+                <h4>Overview</h4>
+                <hr/>
                 ${overview}
             </div> 
         
